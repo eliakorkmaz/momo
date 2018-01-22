64ebf6dedfaf710b451f576510a128779123c44a
Emrah Korkmaz <emrahkrkmz1@gmail.com>
2018-01-22 14:09:13 +0300

    Not important change, only add semi-colon

--- script/helper.js
+++ script/helper.js
@@ -1,9 +1,9 @@
 $('#testimony-link').click(function () {
     var top = $("#testimony-momo").position().top;
     $(window).scrollTop( top );
-})
+});
 
 $('#about-link').click(function () {
     var top = $("#about-momo").position().top;
     $(window).scrollTop( top );
-})
+});
